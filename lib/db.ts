08b12502827d09f7cb7b0e005a5bd723b171dbf7
jayshri <jayshripawar05@gmail.com>
2025-08-29 16:59:24 +0530

    chnage db name

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: "auth-db1328.hstgr.io", // or your DB host
   user: "u528162936_jayshripawar05",      // your DB user
   password: "Jayshri@1995",      // your DB password
-  database: "zaytoon_lms", // your DB name
+  database: "u528162936_zaytoon_lms", // your DB name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
